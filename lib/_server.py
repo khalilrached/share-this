9b352c200b61aa53cafc8e59273ee511ac826fb2
khalilrached <khalilrached058@gmail.com>
2023-02-24 01:00:27 +0100

    add ServerFactory class / change Server to ServerHandler / add class server

--- lib/_server.py
+++ lib/_server.py
@@ -1,16 +1,14 @@
-import json
 import mimetypes
 import os
+from threading import Thread
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from lib import LoggerBuilder
 
 logger = LoggerBuilder.getLogger(os.path.basename(__file__).removesuffix(".py"))
 
-logger.trace(f"%SERVER_HOME%={os.environ['SERVER_HOME']}")
-logger.trace(f"%SERVER_PUBLIC%={os.environ['SERVER_HOME']}")
 
+class ServerHandler(BaseHTTPRequestHandler):
 
-class Server(BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == "/":
             self.path = "/index.html"
@@ -35,8 +33,38 @@ class Server(BaseHTTPRequestHandler):
         self.wfile.write(file)
 
 
-if __name__ == "__main__":
-    SERVER = "localhost"
-    PORT = 5000
-    httpd = HTTPServer((SERVER, PORT), Server)
-    httpd.serve_forever()
+class ServerFactory:
+    host_name: str = None
+    port: int = None
+
+    @staticmethod
+    def create_server(hostname='localhost', port=5000):
+        ServerFactory.port = port
+        ServerFactory.host_name = hostname
+        Server.httpd = HTTPServer((hostname, port), ServerHandler)
+        return Server()
+
+
+class Server:
+    httpd: HTTPServer = None
+    thread: Thread = None
+
+    @staticmethod
+    def serve():
+        try:
+            Server.httpd.serve_forever()
+        except Exception as ex:
+            logger.error(type(ex))
+
+    @staticmethod
+    def start_server():
+        logger.debug("starting the server. ")
+        Server.thread = Thread(target=Server.serve)
+        Server.thread.start()
+        logger.info(f"server is ready http://{ServerFactory.host_name}:{ServerFactory.port}/.")
+
+    @staticmethod
+    def close_server():
+        logger.debug("closing the server. ")
+        Server.thread = None
+        Server.httpd.server_close()
