b30c41e31a45f0aa6ae5f9780d58dabb2e789e18
khalilrached <khalilrached058@gmail.com>
2023-02-27 17:42:00 +0100

    adding options parser bundle

--- lib/__init__.py
+++ lib/__init__.py
@@ -2,6 +2,9 @@ from lib.logger import LoggerBuilder
 from lib.util import toDay
 from lib.util import time_now
 from lib.config import APP_NAME, LOG_DIR, LOG_LEVEL
+from lib.util import get_name
+from lib.util import parse_options
+from lib._server import ServerFactory
 
 from lib.file_manager import compress_directory
 
--- lib/util/__init__.py
+++ lib/util/__init__.py
@@ -1,3 +1,5 @@
 from lib.util._time import toDay
 from lib.util._time import time_now
 from lib.util.file_name import get_file_name
+from lib.util.parser import get_name
+from lib.util.parser import parse_options
