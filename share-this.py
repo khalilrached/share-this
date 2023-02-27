db43ec2bfb8b283b2ef7de57d6dbd7b0f5db1a77
khalilrached <khalilrached058@gmail.com>
2023-02-27 17:42:47 +0100

    implementing server:start command

--- share-this.py
+++ share-this.py
@@ -2,7 +2,7 @@ import os
 import platform
 from sys import *
 
-from lib import LoggerBuilder
+from lib import LoggerBuilder, parse_options, ServerFactory
 
 
 # init logger
@@ -17,8 +17,17 @@ if __name__ == '__main__':
     if "--version" in argv:
         logger.info("version")
         exit(0)
-    if "server:start" in argv:
-        raise Exception("server:start is not implemented. ")
+    if "server:start" in argv: # start the server
+        argv = argv[1:]
+        options = parse_options(argv)
+        hostname = "localhost"
+        port = 6540
+        if options.get("host") is not None:
+            hostname = options.get("host")
+        if options.get("port") is not None:
+            port = options.get("port")
+        server = ServerFactory.create_server(hostname, port)
+        server.start_server()
     if "server:stop" in argv:
         raise Exception("server:stop is not implemented. ")
     if "files:list" in argv:
